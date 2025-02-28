01509d202878d1cc731625f5fa96ec0050cee159
Yanmoree <starostin2006yaroslav@gmail.com>
2025-02-28 16:54:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert ("Hello World!");
+
+window.onclick = (event) => {
+    ['id01', 'id02'].forEach(id => {
+      const modal = document.getElementById(id);
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  };
+  
